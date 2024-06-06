693dda2bf242b4816c84cea552f0f6e33b568ea8
brandondando <dandobrandon0@gmail.com>
2024-06-06 16:19:11 +0300

    env done away with

---
+++ lib/db/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    transactions = relationship('Transaction', back_populates='user')
+    goals = relationship('Goal', back_populates='user')
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    amount = Column(Float)
+    date = Column(Date)
+    description = Column(String)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    user = relationship('User', back_populates='transactions')
+    category = relationship('Category', back_populates='transactions')
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    transactions = relationship('Transaction', back_populates='category')
+
+class Goal(Base):
+    __tablename__ = 'goals'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    amount = Column(Float)
+    description = Column(String)
+    user = relationship('User', back_populates='goals')
